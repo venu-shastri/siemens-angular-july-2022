1f697b6a94ae4fb6ca012240c5b6391b2d45639c
venu-shastri <venu.shastri@gmail.com>
2022-06-28 10:50:19 +0530

    New Project

--- angular-building-blocks/src/root/components/root.component.ts
+++ angular-building-blocks/src/root/components/root.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     templateUrl:'./root.component.html',
-    styleUrls:['./root.component.css']
+    styleUrls:['./root.component.css'],
+    selector:'root-component'
 })
 export class RootComponent{
   //Logic
